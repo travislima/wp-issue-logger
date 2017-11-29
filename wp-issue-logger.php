2a7af56080e389c18931132d5d03f7fb816370ed
jmfloor <33619163+jmfloor@users.noreply.github.com>
2017-11-29 08:58:45 +0200

    wp-issue-logger plugin file

---
+++ wp-issue-logger.php
@@ -0,0 +1,12 @@
+<?php
+/*
+Plugin Name:  WP Issue Logger
+Description:  Basic WordPress Plugin to log issues
+Version:      0.1
+Author:       Travis Lima, Johannes Floor
+Author URI:   travislima.com
+License:      GPL2
+License URI:  https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain:  wpil
+Domain Path:  /languages
+*/

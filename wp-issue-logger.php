b0dbd62312d8d029c9ff13b756406a73afdcf9fc
jmfloor <33619163+jmfloor@users.noreply.github.com>
2017-11-29 10:03:30 +0200

    Update wp-issue-logger.php

--- wp-issue-logger.php
+++ wp-issue-logger.php
@@ -10,3 +10,20 @@ License URI:  https://www.gnu.org/licenses/gpl-2.0.html
 Text Domain:  wpil
 Domain Path:  /languages
 */
+
+/*
+//EXit if accessed directly
+if ( ! defined('ABSPATH')) {
+    exit;
+}
+*/
+
+function wpil_register_post_type() {
+
+    $args = array( 'public' => true, 'label' => 'WP Issue Logger' );
+
+    register_post_type( 'issue', $args);
+
+}
+
+add_action( 'init', 'wpil_register_post_type');

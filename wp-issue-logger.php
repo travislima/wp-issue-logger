77aa35a1c8042db1352ea12f7458fe3a668abbfb
Travis Lima <hello@travislima.com>
2017-11-30 09:23:47 +0200

    Added Labels to CPT
    Added Labels to CPT

--- wp-issue-logger.php
+++ wp-issue-logger.php
@@ -20,7 +20,30 @@ if ( ! defined('ABSPATH')) {
 
 function wpil_register_post_type() {
 
-    $args = array( 'public' => true, 'label' => 'WP Issue Logger' );
+
+		$labels = array(
+        'name'                => _x( 'Issues', 'Post Type General Name' ),
+        'singular_name'       => _x( 'Issue', 'Post Type Singular Name' ),
+        'menu_name'           => __( 'WP Issue Logger' ),
+        'parent_item_colon'   => __( 'Main Issue'),
+        'all_items'           => __( 'All Issues' ),
+        'view_item'           => __( 'View Issue'),
+        'add_new_item'        => __( 'Add New Issue' ),
+        'add_new'             => __( 'Add New' ),
+        'edit_item'           => __( 'Edit Issue' ),
+        'update_item'         => __( 'Update Issue' ),
+        'search_items'        => __( 'Search Issue' ),
+        'not_found'           => __( 'Not Found' ),
+        'not_found_in_trash'  => __( 'Not found in Trash' ),
+    );
+
+    $args = array( 
+
+    	'public' => true, 
+    	'label' => 'WP Issue Logger',
+    	'labels' => $labels
+
+    );
 
     register_post_type( 'issue', $args);
 

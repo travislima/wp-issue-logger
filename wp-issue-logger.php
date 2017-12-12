41d9a735970b3604962ad4b435a4a03f0e0c5f2c
Travis Lima <hello@travislima.com>
2017-12-12 11:54:34 +0200

    Final Release Changes (almost)
    Made strings translatable
    Added Metabox data save feature
    Removed Single & Archive templates.

--- wp-issue-logger.php
+++ wp-issue-logger.php
@@ -7,10 +7,9 @@ Author:       Travis Lima, Johannes Floor
 Author URI:   travislima.com
 License:      GPL2
 License URI:  https://www.gnu.org/licenses/gpl-2.0.html
-Text Domain:  wpil
+Text Domain:  wp-issue-logger
 Domain Path:  /languages
 */
-
 
 //EXit if accessed directly
 if ( ! defined('ABSPATH')) {
@@ -18,47 +17,41 @@ if ( ! defined('ABSPATH')) {
 }
 
 require_once ( plugin_dir_path(__FILE__) . 'wp-issue-logger-shortcode.php');
-
-
 function wpil_register_post_type() {
-
-
 		$labels = array(
-        'name'                => _x( 'Issues', 'Post Type General Name' ),
-        'singular_name'       => _x( 'Issue', 'Post Type Singular Name' ),
-        'menu_name'           => __( 'WP Issue Logger' ),
-        'parent_item_colon'   => __( 'Main Issue'),
-        'all_items'           => __( 'All Issues' ),
-        'view_item'           => __( 'View Issue'),
-        'add_new_item'        => __( 'Add New Issue' ),
-        'add_new'             => __( 'Add New' ),
-        'edit_item'           => __( 'Edit Issue' ),
-        'update_item'         => __( 'Update Issue' ),
-        'search_items'        => __( 'Search Issue' ),
-        'not_found'           => __( 'Not Found' ),
-        'not_found_in_trash'  => __( 'Not found in Trash' ),
+        'name'                => __( 'Issues', 'wp-issue-logger' ),
+        'singular_name'       => __( 'Issue', 'wp-issue-logger' ),
+        'menu_name'           => __( 'WP Issue Logger', 'wp-issue-logger' ),
+        'parent_item_colon'   => __( 'Main Issue', 'wp-issue-logger' ),
+        'all_items'           => __( 'All Issues', 'wp-issue-logger' ),
+        'view_item'           => __( 'View Issue', 'wp-issue-logger' ),
+        'add_new_item'        => __( 'Add New Issue', 'wp-issue-logger' ),
+        'add_new'             => __( 'Add New', 'wp-issue-logger' ),
+        'edit_item'           => __( 'Edit Issue', 'wp-issue-logger' ),
+        'update_item'         => __( 'Update Issue', 'wp-issue-logger' ),
+        'search_items'        => __( 'Search Issue', 'wp-issue-logger' ),
+        'not_found'           => __( 'Not Found', 'wp-issue-logger' ),
+        'not_found_in_trash'  => __( 'Not found in Trash', 'wp-issue-logger' ),
     );
 
     $args = array( 
-
     	'public' => true, 
-    	'label' => 'WP Issue Logger',
+    	'label' => __( 'WP Issue Logger', 'wp-issue-logger' ),
     	'labels' => $labels,
         'show_in_admin_bar' => true,
         'menu_icon' => 'dashicons-image-filter',
         'capability_type' => 'post',
         'map_meta_cap' => true,
         'has_archive' => true,
-        'register_meta_box_cb' => 'wpt_add_event_metaboxes',
+        'register_meta_box_cb' => 'wpil_add_meta_box',
         'rewrite'     => array (
-            'slug' => 'events',
+            'slug' => 'issues',
             'with_front' => true,
             'pages' => true,
             'feeds' => true
         
         ),
       
-
         'supports' => array(
             'title',
             'editor',
@@ -66,98 +59,100 @@ function wpil_register_post_type() {
             'revisions',
             'author', 
             'page-attributes',
-          //  'thumbnail',
-            'custom-fields',
-           // 'post-formats'
         )
     );
-
     register_post_type( 'issue', $args);
-
 }
 
 add_action( 'init', 'wpil_register_post_type');
 
 
 // Flush Permalink on Plugin activation.
-
 function wpil_rewrite_flush() {
-    // First, we "add" the custom post type via the above written function.
-    // Note: "add" is written with quotes, as CPTs don't get added to the DB,
-    // They are only referenced in the post_type column with a post entry, 
-    // when you add a post of this CPT.
-    my_cpt_init();
-
-    // ATTENTION: This is *only* done during plugin activation hook in this example!
-    // You should *NEVER EVER* do this on every page load!!
+    wpil_register_post_type();
     flush_rewrite_rules();
 }
 register_activation_hook( __FILE__, 'wpil_rewrite_flush' );
+register_deactivation_hook( __FILE__, 'wpil_rewrite_flush' );
 
-
-
-//override archive-issue page with custom page from plugin folder
-
-/** Enqueue CSS */
-add_action( 'wp_enqueue_scripts', 'prefix_add_my_stylesheet' );
-
-function prefix_add_my_stylesheet() {
-   wp_register_style( 'cpt-style', plugins_url( 'css/style.css', __FILE__) );
-   wp_enqueue_style( 'cpt-style' );
-}
-
-// force use of templates from plugin folder
-function cpte_force_template( $template )
-{	
-    if( is_archive( 'issue' ) ) {
-        $template = WP_PLUGIN_DIR .'/'. plugin_basename( dirname(__FILE__) ) .'/archive-issue.php';
-	}
-
-	/* if( is_singular( 'issue' ) ) {
-        $template = WP_PLUGIN_DIR .'/'. plugin_basename( dirname(__FILE__) ) .'/single-issue.php';
-	}
- */
-    return $template;
-}
-add_filter( 'template_include', 'cpte_force_template' );
-
-
-
-/*===================================================================================*/
 
 /**
- * Adds a metabox to the right side of the screen under the â€œPublishâ€ box
- */
-function wpt_add_event_metaboxes() {
+ * Add issue status taxonomy
+*/
+add_action( 'init', 'create_issueCategory_tax' );
+function create_issueCategory_tax() {
+    $taxlabels = array(
+		'name'              => __( 'Issue Categories', 'wp-issue-logger' ),
+		'singular_name'     => __( 'Issue Category', 'wp-issue-logger'  ),
+		'search_items'      => __( 'Search Categories', 'wp-issue-logger'  ),
+		'all_items'         => __( 'All Categories', 'wp-issue-logger' ),
+		'parent_item'       => __( 'Parent Category', 'wp-issue-logger' ),
+		'parent_item_colon' => __( 'Parent Category:', 'wp-issue-logger' ),
+		'edit_item'         => __( 'Edit Category', 'wp-issue-logger' ),
+		'update_item'       => __( 'Update Category', 'wp-issue-logger' ),
+		'add_new_item'      => __( 'Add New Category', 'wp-issue-logger' ),
+		'new_item_name'     => __( 'New Category Name', 'wp-issue-logger' ),
+        'menu_name'         => __( 'Categories', 'wp-issue-logger' ),
+    );
+
+	register_taxonomy(
+		'type',
+		'issue',
+		array(
+            'label' => 'Issue Category',
+            'labels' => $taxlabels,
+			'rewrite' => array( 'slug' => 'issue-category' ),
+            'hierarchical' => true,
+            'description' => 'Categorise your issues with different tags suchs as "New Features", "Bug", "Errors"'
+		)
+	);
+}
+
+/*============Adds Metabox to the right side of the CPT Editor=========*/
+
+
+function wpil_add_meta_box() {
     add_meta_box(
-        'wpt_events_location',
-        'Event Location',
-        'wpt_events_location',
+        'wpil_issue_status',
+        __( 'Issue Status', 'wp-issue-logger' ),
+        'wpil_issue_status_show_metabox',
         'issue',
         'side',
         'default'
-
     );
 }
-
-
 
 /**
  * Output the HTML for the metabox.
  */
-function wpt_events_location() {
+function wpil_issue_status_show_metabox() {
     global $post;
-    // Nonce field to validate form request came from current site
-    wp_nonce_field( basename( __FILE__ ), 'event_fields' );
+    // **** CREATE NONCE AND CHECK IT / NOT NEEDED ATM THOUGH ****
     // Get the location data if it's already been entered
-    $location = get_post_meta( $post->ID, 'status', true );
-    // Output the field
-    echo /*'<input  name="location" placeholder="Example - Text" value="' . esc_textarea( $location )  . '" class="widefat">*/
+    $issue_status = get_post_meta( $post->ID, 'wpil_status', true );
+    ?>
 
-    '<input type="radio" name="status" value="new"> New
-    <input type="radio" name="status" value="in_progress"> In Progress
-    <input type="radio" name="status" value="completed"> Completed';
+<!-- Selection options for Metabox -->
+    <select name="wpil-issue-status" id="wpil-issue-status">
+        <option value="new" <?php selected( 'new', $issue_status, true); ?> >New</option>
+        <option value="in-progress" <?php selected( 'in-progress', $issue_status, true); ?> >In Progress</option>
+        <option value="completed" <?php selected( 'completed', $issue_status, true); ?> >Completed</option>
+    </select>
+    <?php
 }
 
+/*============Saves METABOX Data=========*/
 
-add_action( 'add_meta_boxes', 'wpt_add_event_metaboxes' );
+add_action( 'add_meta_boxes', 'wpil_add_meta_box' );
+/* Save post meta on the 'save_post' hook. */
+add_action( 'save_post', 'smashing_save_post_class_meta', 10, 2 );
+/* Save the meta box's post metadata. */
+function smashing_save_post_class_meta( $post_id, $post ) {
+    // check to see if post_type is 'issue', if not return $post_id;
+    $status = $_REQUEST['wpil-issue-status'];
+    if( !empty( $status ) ){
+        update_post_meta( $post_id, 'wpil_status', $status );
+    }else{
+        delete_post_meta( $post_id, 'wpil_status' );
+    }
+}
